23eef00a103c9bf873f0a00d39690735d3e3f760
Anatoly_Karpovich <Anatoly.Karpovich@coxautoinc.com>
2023-10-10 03:26:50 +0200

    Added credentials interface

--- src/ui/steps/login.steps.ts
+++ src/ui/steps/login.steps.ts
@@ -1,8 +1,9 @@
+import { ICredentials } from "../../types/login.types.js";
 import AllDashboardsPage from "../pages/dashboards/allDashboards.page.js";
 import LoginPage from "../pages/login.page.js";
 
 class LoginSteps {
-    async login(credentials?: { login: string, password: string }) {
+    async login(credentials?: ICredentials) {
         await LoginPage.setValue(LoginPage["Login input"], credentials?.login || process.env!.LOGIN || "")
         await LoginPage.setValue(LoginPage["Password input"], credentials?.password || process.env.PASSWORD || "")
         await LoginPage.click(LoginPage["Login button"])
